171f58aab695832a3718b3c99eaee52e9fbe7434
shhemanthsnowflake <132969112+shhemanthsnowflake@users.noreply.github.com>
2023-05-10 07:39:23 -0400

    Update streamlit_app.py
    Making improvements like adding emoji and new Breakfast to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
