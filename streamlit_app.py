709de7a5d1d589166d14e2ceb01d78bd5f3e1f28
shhemanthsnowflake <132969112+shhemanthsnowflake@users.noreply.github.com>
2023-05-10 07:44:51 -0400

    Update streamlit_app.py
    Adding Pandas Library and importing CSV from S3 Bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
+import pandas as pd
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
