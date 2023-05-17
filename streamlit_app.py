f41d070e23d23d926aa1368793c7f7e4085fb7ef
shhemanthsnowflake <132969112+shhemanthsnowflake@users.noreply.github.com>
2023-05-16 20:47:29 -0400

    Update streamlit_app.py
    Adding function

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,16 +17,21 @@ fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 # display the table on page
 streamlit.dataframe(fruits_to_show)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+   #streamlit.text(fruityvice_response.json())
+   fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+#New section to display fruityvice api response   
 streamlit.header('Fruityvice Fruit Advice!')
 try:
    fruit_choice=streamlit.text_input('what fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("please select a fruit to get information")
    else:
-      fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-      #streamlit.text(fruityvice_response.json())
-      fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function=get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)     
+      
 except URLError as e:
    streamlit.error()
 streamlit.write('The user entered',fruit_choice)
