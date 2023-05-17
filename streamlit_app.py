8896cb67af31795fd614824949437929d6e380f6
shhemanthsnowflake <132969112+shhemanthsnowflake@users.noreply.github.com>
2023-05-16 20:58:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,14 +35,20 @@ try:
 except URLError as e:
    streamlit.error()
 streamlit.write('The user entered',fruit_choice)
-# don't run anything past here while we trouble shoot
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#Snowflake related functions
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+      return my_cur.fetchall()
+ # Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
+# don't run anything past here while we trouble shoot
+streamlit.stop()  
+
 add_fruit=streamlit.text_input('what fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding',add_fruit)
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
